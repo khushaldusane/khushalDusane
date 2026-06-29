54b88dbdb411ed714f598ec93c3dd91cd928ed23
khushaldusane <155304237+khushaldusane@users.noreply.github.com>
2026-06-29 10:37:23 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -35,3 +35,16 @@ l2=[2,5,6,7,9]
 m=l1+l2
 m.sort()
 print(m)
+
+# Question:
+# With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+n=int(input("Enter a number "))
+d={}
+for i in range(1,n+1):
+  d[i]=i*i
+print(d)
